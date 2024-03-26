8bcdee50b3a546926ab1a014f5578e4db5a861f0
Joseju002 <126481238+Joseju002@users.noreply.github.com>
2024-03-26 12:49:35 +0100

    registro (prueba) & permisos de admin y user

--- public/js/manejo_sesion.js
+++ public/js/manejo_sesion.js
@@ -20,14 +20,14 @@ function iniciarSesion() {
         if (data.res == "login true") { //Si el login es exitoso
             window.location.replace("/ruta-prueba");
         } else if (data.res == "login invalid") { //Si no es exitoso
-            mensaje.innerHTML = `<div class="mt-2 text-center alert alert-danger p-2" id="mensaje">
+            mensaje.innerHTML = `<div class="mt-2 border-danger text-center alert alert-danger p-2" id="mensaje">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
                     <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
                 </svg>
                 <b>Error:</b> el usuario no existe o la contraseña es incorrecta.
             </div>`
         } else if (data.res == "login failed") { //Ha faltado un parametro
-            mensaje.innerHTML = `<div class="mt-2 text-center alert alert-danger p-2" id="mensaje">
+            mensaje.innerHTML = `<div class="mt-2 border-danger text-center alert alert-danger p-2" id="mensaje">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
                     <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
                 </svg>
@@ -39,3 +39,14 @@ function iniciarSesion() {
     });
 
 }
+
+//Para que al presionar Enter se envíe la información
+function handleEnterKeyPress(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById('submit').click();
+    }
+}
+document.getElementById('formulario').addEventListener('keypress', function(event) {
+    handleEnterKeyPress(event);
+});
--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 //Importación de las librerías
 const express = require('express')
 const fs = require('fs')
+var session = require('express-session');
 
 //Creación de la aplicación
 const app = express()
 app.use(express.static('public'))
 app.use(express.json());
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}))
+
+var auth = function (req, res, next) {
+    if (req.session && (req.session.admin || req.session.user)) {
+        return next();
+    } else {
+        res.redirect('/iniciarSesion');
+    }
+}
 
 //Conexión a la base de datos
 const pool = require('./db/db_sql.js')
@@ -27,7 +42,17 @@ app.get('/registrarUsuario', (req, res) => {
     res.send(contenido)
 })
 app.get('/ruta-prueba', (req, res) => {
-    res.send('<h1>Ruta protegida</h1>')
+    if (req.session.admin) {
+        res.send('<h1>Bienvenido, administrador</h1>')
+    } else if (req.session.user) {
+        res.send('<h1>Bienvenido, usuario</h1>')
+    } else {
+        res.redirect('/iniciarSesion');
+    }
+})
+app.get('/destruir', (req, res) => {
+    req.session.destroy();
+    res.redirect('/iniciarSesion');
 })
 
 //Consultas BBDD
@@ -35,13 +60,19 @@ app.get('/consulta', (req, res) => {
     pool.getConnection((err, connection) => {
         if (err) throw err
         console.log('Conectado a la base de datos MySQL')
-        connection.query('SELECT * FROM usuarios', (err, rows) => {
+        connection.query('SELECT * FROM prueba', (err, rows) => {
             console.log('- Consulta realizada -')
             connection.release() // Devolver la conexión a la pool
             console.log('Desconexión de la base de datos MySQL')
 
             if (!err) {
-                res.json(rows);
+                if (rows.length > 0) {
+                    const campo3Value = rows[0].campo3.toString();
+                    res.send(campo3Value);
+                } else {
+                    res.send('No se encontraron resultados');
+                }
+                res.end();
             } else {
                 console.log(err)
             }
@@ -65,7 +96,18 @@ app.post('/iniciar', async (req, res) => {
                         console.error('Error al ejecutar la consulta: ', error);
                     } else {
                         if (results.length > 0) {
-                            res.send({ "res": "login true"});
+                            if (results.length > 0) {
+                                const esAdmin = parseInt(results[0].admin);
+                                if(esAdmin === 1){
+                                    req.session.admin = true;
+                                    console.log("Entró un administrador")
+                                } else {
+                                    req.session.user = true;
+                                    console.log("Entró un usuario")
+                                }
+
+                            }
+                            res.send({ "res": "login true" });
                         } else {
                             res.send({ "res": "login invalid" });
                         }
@@ -75,6 +117,40 @@ app.post('/iniciar', async (req, res) => {
         });
     }
 })
+
+app.post('/registrar', async (req, res) => {
+    if (!req.body.username || !req.body.password || !req.body.email) {
+        res.send({ "res": "register failed" })
+    } else {
+        pool.getConnection(function (err, connection) {
+            if (err) {
+                console.error('Error al obtener la conexión de la pool: ', err);
+                res.send({ "res": "register failed" });
+            } else {
+                connection.query('INSERT INTO usuarios(username, email, password, admin) VALUES (?,?,?, false)', [req.body.username, req.body.email, req.body.password], function (error, results, fields) {
+                    connection.release(); // Liberar la conexión después de usarla
+
+                    if (error) {
+                        if (error.code === 'ER_DUP_ENTRY') {
+                            // Manejar el error de clave duplicada aquí
+                            res.send({ "res": "user exists" });
+                        } else {
+                            console.error('Error al ejecutar la consulta: ', error);
+                        }
+                    } else {
+                        if (results.affectedRows > 0) {
+                            res.send({ "res": "register true" });
+                        } else {
+                            res.send({ "res": "register invalid" });
+                        }
+                    }
+                });
+            }
+        });
+    }
+})
+
+
 
 //En el caso de que vayamos a una ruta que no existe, irá a la raiz por defecto
 app.use((req, res) => {
